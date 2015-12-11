821653ab6162e4c89ae3d76bff3f6b996ac7e5b8
Marcin Drzewiecki <marcin.drzewiecki@gmail.com>
2015-12-11 19:05:26 +0100

    adding login option in html - only UI

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 var app = angular.module('BookEaterClub', []);
+
+
 app.controller('PastBooks', function($scope) {
          $scope.books = [
         {
@@ -51,6 +53,8 @@ app.controller('PastBooks', function($scope) {
         }
     ];
     });
+
+
 app.controller('NowRead', function($scope) {
     $scope.book = 
         {
@@ -65,6 +69,8 @@ app.controller('NowRead', function($scope) {
           pub: 'XXX'
         };
 });
+
+
 app.controller('About', function($scope) {
 
          $scope.about = [
@@ -96,6 +102,52 @@ app.controller('About', function($scope) {
         
     ];
     });
-    
+
+
+app.controller('FundooCtrl', function($scope, $window) {
+    $scope.rating = 5;
+    $scope.saveRatingToServer = function(rating) {
+      /*$window.alert('Rating selected - ' + rating);<--- need to put some code here*/
+    };
+  })
+  .directive('fundooRating', function () {
+    return {
+      restrict: 'A',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '=',
+        max: '=',
+        readonly: '@',
+        onRatingSelected: '&'
+      },
+      link: function (scope, elem, attrs) {
+
+        var updateStars = function() {
+          scope.stars = [];
+          for (var  i = 0; i < scope.max; i++) {
+            scope.stars.push({filled: i < scope.ratingValue});
+          }
+        };
+
+        scope.toggle = function(index) {
+          if (scope.readonly && scope.readonly === 'true') {
+            return;
+          }
+          scope.ratingValue = index + 1;
+          scope.onRatingSelected({rating: index + 1});
+        };
+
+        scope.$watch('ratingValue', function(oldVal, newVal) {
+          if (newVal) {
+            updateStars();
+          }
+        });
+      }
+    }
+  });
     
     
